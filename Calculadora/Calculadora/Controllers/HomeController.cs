ba83c8de01f30af2003b1c16e03e255d7785767e
RicMFM <aluno23306@ipt.pt>
2022-03-14 22:24:13 +0000

    calculadora

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -4,29 +4,63 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Calculadora.Controllers
 {
-    public class HomeController : Controller
-    {
+    public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
+        public HomeController(ILogger<HomeController> logger) {
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
+
+        [HttpGet] // esta anotação é facultativa
+        public IActionResult Index() {
+
+            // inicialiazar a calculadora
+            ViewBag.Visor = "0";
+
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+        [HttpPost] // mas, esta anotação já é obrigatória, para o método 'escutar' o HTTP POST
+        public IActionResult Index(string botao, string visor) {
+
+
+            switch (botao) {
+
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    // atribuir ao VISOR o algorismo selecionado
+                    if(visor != "0") visor = visor + botao;
+                    else { visor = botao; }
+                    
+                    break;
+            }
+
+            //preparar dados a serem enviados para a View
+            ViewBag.Visor = visor;
+
+
+            return View();
+            }
+
+
+
+            public IActionResult Privacy() {
+                return View();
+            }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            public IActionResult Error() {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
         }
     }
-}
